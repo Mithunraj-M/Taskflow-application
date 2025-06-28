ab4f4a3d19caa04ef2f9eef6b642ded426eca6b1
Mithunraj M <mithunraj.m06@gmail.com>
2025-06-28 19:39:55 +0530

    setup tailwind.css

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Dashboard() {
-  return <h1>Welcome to your Dashboard 🚀</h1>;
+  return (
+    <div>
+      <h1>Welcome to your Dashboard 🚀</h1>
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
 }
 
 export default Dashboard;
